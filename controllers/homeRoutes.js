417061a9460cfd574b39579dbd1fdae4d7c1c883
Richard You <richardyou94@gmail.com>
2022-11-11 13:47:25 -0800

    homepage routes success

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -16,7 +16,7 @@ router.get('/', async (req, res) => {
         const blogs = blogData.map((blog) => blog.get({ plain: true }));
 
         res.render('homepage', {
-            projects,
+            blogs,
             logged_in: req.session.logged_in
         });
     } catch (err) {
--- server.js
+++ server.js
@@ -2,14 +2,13 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-// Make const routes from controller folders
-
+const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 const hbs = exphbs.create({ helpers });
 
