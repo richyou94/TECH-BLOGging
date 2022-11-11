a583f04930ad7545507e6543f5cd173a99543fa3
Richard You <richardyou94@gmail.com>
2022-11-11 13:18:46 -0800

    added the sample data in json format for seeds

--- models/index.js
+++ models/index.js
@@ -1,3 +1,13 @@
 const User = require('./User');
+const Blog = require('./Blog');
 
-module.exports = { User };
+User.hasMany(Blog, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Blog.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+module.exports = { User, Blog };
