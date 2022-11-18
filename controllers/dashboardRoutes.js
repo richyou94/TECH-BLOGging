02138fea558dcedd77d4770b0ba029823cc51146
Richard You <richardyou94@gmail.com>
2022-11-18 09:52:22 -0800

    add separate dashboard Routes

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+const { Blog, User } = require("../models");
+const withAuth = require("../utils/auth");
+
+router.get("/", withAuth, async (req, res) => {
+    try {
+      const userId = req.session.user_id;
+      const blogData = await Blog.findAll({
+        include: [
+            {
+                model: User,
+                attributes: ['username'],
+            },
+        ],
+        where: { user_id: userId },
+      });
+      const blogs = blogData.map((blog) => blog.get({ plain: true }));
+  
+      res.render("dashboard", {
+        blogs,
+        logged_in: req.session.logged_in,
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -24,29 +24,6 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/dashboard", async (req, res) => {
-  try {
-    const userId = req.session.user_id;
-    const blogData = await Blog.findAll({
-      // where: { user_id: userId },
-      // include: [
-      //     {
-      //         model: User,
-      //         attributes: ['username'],
-      //     },
-      // ],
-    });
-    const blogs = blogData.map((blog) => blog.get({ plain: true }));
-
-    res.render("dashboard", {
-      blogs,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
 router.get("/login", async (req, res) => {
   try {
     if (req.session.logged_in) {
